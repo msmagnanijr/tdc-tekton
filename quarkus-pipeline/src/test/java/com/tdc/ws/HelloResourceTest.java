19034bdc28ca7136f94a2030dd73d35028ffd0e9
Bruno Dias <bruno.dias@rockt.com.br>
2019-11-26 20:58:56 -0300

    Inclusão da app junto com o pipeline

---
+++ quarkus-pipeline/src/test/java/com/tdc/ws/HelloResourceTest.java
@@ -0,0 +1,22 @@
+package com.tdc.ws;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class HelloResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/api")
+          .then()
+             .statusCode(200)
+             .body(containsString("Huhu") , containsString("6bbded"));
+    }
+
+}
